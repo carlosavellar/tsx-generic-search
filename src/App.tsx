2319597b7fd69205382e5bed88f7e169343139b9
carlos.avellar <carlos.avellar@gmail.com>
2023-04-12 21:03:41 -0300

    form searchQeury

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { widget } from './mock/widget';
+import { genericSearch } from './utils/genericSearch';
+import { SearchForm } from './components/SearchForm';
+import { IWidget } from './interfaces/widgets';
 
 function App() {
-  return <div className="App">w..</div>;
+  const [query, setQuery] = useState<string>('');
+
+  return (
+    <div>
+      <SearchForm setSearchQuery={setQuery} query={query} />
+      <h3>Widget</h3>
+      <ul>
+        {widget
+          .filter((property) => genericSearch(property, ['title', 'description'], query, true))
+          .map((wid) => {
+            return <li>{wid.title}</li>;
+          })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
