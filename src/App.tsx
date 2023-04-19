c163666ae83cf28b87186362a85e2412e968a947
carlos.avellar <carlos.avellar@gmail.com>
2023-04-19 15:47:05 -0300

    Generic sort

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import * as generics from './generics/generic';
+
+console.log(generics.sortByBar(generics.fooBar));
+console.log(generics.sortByFoo(generics.fooBar));
+console.log(generics.genericSort(generics.fooBar, 'bar'));
+
 function App() {
   return <div className="App">w..</div>;
 }
---
+++ src/generics/generic.ts
@@ -0,0 +1,35 @@
+import React from 'react';
+
+interface FooBar {
+  foo: string;
+  bar: string;
+}
+
+export const fooBar: Array<FooBar> = [
+  { foo: 'Aoo', bar: 'bar' },
+  { foo: 'Xoo', bar: 'far' },
+  { foo: 'O3oo', bar: 'rtar' },
+];
+
+export const sortByFoo = (data: Array<FooBar>) => {
+  return data.sort((a, b) => {
+    if (a.foo < b.foo) return -1;
+    if (a.foo > b.foo) return 1;
+    else return 0;
+  });
+};
+export const sortByBar = (data: Array<FooBar>) => {
+  return data.sort((a, b) => {
+    if (a.foo < b.foo) return -1;
+    if (a.foo > b.foo) return 1;
+    else return 0;
+  });
+};
+
+export const genericSort = <T>(data: Array<T>, key: keyof T) => {
+  return data.sort((a, b) => {
+    if (a[key] < b[key]) return -1;
+    if (a[key] > b[key]) return 1;
+    else return 0;
+  });
+};
